84b1f321de61d47ab5d745587fc61e67967eb98b
dimas14999 <dima.arxarow@gmail.com>
2023-10-23 19:20:42 +0300

    Начал делать первую логику

--- CTP/Assets/Project/Scripts/UI/LobbyView.cs
+++ CTP/Assets/Project/Scripts/UI/LobbyView.cs
@@ -1,14 +1,29 @@
 using RedPanda.Project.Services;
+using RedPanda.Project.Services.Interfaces;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace RedPanda.Project.UI
 {
     public sealed class LobbyView : View
     {
+        private const string LOBBY_VIEW = "LobbyView";
+        private const string PROMOS_VIEW = "PromoView";
+        
+        [SerializeField] private Button _startButton;
         private void Awake()
         {
+            _startButton.onClick.AddListener(ShowPromos);
             //Example for services
             //var promoService = Container.Locate<IPromoService>();
-            //UIService.Close();
+          //  promoService.GetPromos();
+          
+        }
+
+        private void ShowPromos()
+        {
+            UIService.Close(LOBBY_VIEW);
+            UIService.Show(PROMOS_VIEW);
         }
     }
 }
---
+++ CTP/Assets/Project/Scripts/UI/PromoView.cs
@@ -0,0 +1,11 @@
+
+using UnityEngine;
+
+
+namespace RedPanda.Project.UI
+{
+    public class PromoView : View
+    {
+       //[SerializeField] private TMP_Text _coinText;
+    }
+}
